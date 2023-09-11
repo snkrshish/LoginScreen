fe35b82f93071a9d8a67f3c5c6a08a2d32302c40
Nikita Shilyaev <shilyaef@iCloud.com>
2023-09-11 12:46:31 +0800

    add login and password UITextField

--- LoginScreen/Sources/ViewController.swift
+++ LoginScreen/Sources/ViewController.swift
@@ -10,13 +10,34 @@ class ViewController: UIViewController {
         return image
     }()
 
+    private lazy var loginTextField: UITextField = {
+        let textField = UITextField()
+        textField.clipsToBounds = true
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Введите логин или e-mail"
+        textField.font = .systemFont(ofSize: 14)
+        textField.textAlignment = .left
+        textField.backgroundColor = .white
+        return textField
+    }()
+
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.clipsToBounds = true
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Введите пароль"
+        textField.font = .systemFont(ofSize: 14)
+        textField.textAlignment = .left
+        textField.backgroundColor = .white
+        return textField
+    }()
+
 
     //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupHierarchy()
         setupLayout()
-
     }
 
 
@@ -28,10 +49,24 @@ class ViewController: UIViewController {
             $0.leading.equalTo(view)
             $0.trailing.equalTo(view)
         }
+        loginTextField.snp.makeConstraints {
+            $0.leading.equalTo(view).offset(100)
+            $0.trailing.equalTo(view).offset(-100)
+            $0.top.equalTo(view).offset(150)
+            $0.centerX.equalTo(view)
+        }
+        passwordTextField.snp.makeConstraints {
+            $0.centerX.equalTo(view)
+            $0.leading.equalTo(view).offset(100)
+            $0.trailing.equalTo(view).offset(-100)
+            $0.top.equalTo(loginTextField).offset(70)
+        }
     }
 
     private func setupHierarchy() {
         view.addSubview(imageView)
+        view.addSubview(loginTextField)
+        view.addSubview(passwordTextField)
     }
 
     //MARK: - Action
